734bdb932c3a612ac7b87ed45952ea8ab685c7ed
Muhammad <MUhammadkhonn@nycstudents.net>
2025-10-05 20:42:07 +0000

    Sun, Oct 5, 2025, 4:42 PM -04:00

--- tool/p5.play/tinkering.js-p5.js
+++ tool/p5.play/tinkering.js-p5.js
@@ -1,9 +1,11 @@
-console.log("Sketch loaded!"); // for debugging
+console.log("Sketch loaded!");
 
 function setup() {
-  new Canvas(400, 400); // create a canvas
+  new Canvas(400, 400); // p5play function
 
-  let sprite = new Sprite();
-  sprite.text = "Hello!";
-  sprite.color = "blue";
+  let player = new Sprite();
+  player.x = 200;
+  player.y = 200;
+  player.color = 'blue';
+  player.text = 'Hello!';
 }
