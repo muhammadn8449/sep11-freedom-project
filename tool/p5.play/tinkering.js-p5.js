d1335d3d1fd7bb60cc9a5f42ab67d832859c257a
Muhammad <MUhammadkhonn@nycstudents.net>
2025-11-03 17:31:04 +0000

    Mon, Nov 3, 2025, 12:31 PM -05:00

--- tool/p5.play/tinkering.js-p5.js
+++ tool/p5.play/tinkering.js-p5.js
@@ -8,7 +8,7 @@ function setup() {
     sprite.x = x;
     sprite.y = y;
 	}
-	circle(500, 200); circle(200, 100); circle(300, 400); circle(400, 100); circle(500, 100); circle(50, 100);circle(350, 100);circle(600, 100);circle(700, 100);
+	circle(500, 200); circle(500, 300); circle();
 }
 
 function draw() {
